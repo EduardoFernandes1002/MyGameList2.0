8b2942ccc19aa4fe949d1e50333ad8a90342ac25
Eduardo Fernandes <fernandeseduardo1002@gmail.com>
2024-12-15 18:39:10 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { InfoGameComponent } from './pages/info-game/info-game.component';
+import { RankComponent } from './pages/rank/rank.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'info', component: InfoGameComponent },
+  { path: 'rank', component: RankComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'admin', component: AdminComponent }
+];
---
+++ src/app/pages/info-game/info-game.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ReviewComponent } from '../../component/review/review.component';
+
+@Component({
+  selector: 'app-info-game',
+  imports: [ReviewComponent],
+  templateUrl: './info-game.component.html',
+  styleUrl: './info-game.component.css'
+})
+export class InfoGameComponent {
+  
+}
