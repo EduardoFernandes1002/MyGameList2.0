49890d02fd58df4b347a24ea15eaf7272378dc76
EduardoFernandes1002 <fernandeseduardo1002@gmail.com>
2025-03-24 18:14:21 -0300

    To pc S

--- src/app/pages/info-game/info-game.component.ts
+++ src/app/pages/info-game/info-game.component.ts
@@ -3,7 +3,7 @@ import { ReviewComponent } from '../../component/review/review.component';
 
 @Component({
   selector: 'app-info-game',
-  imports: [ReviewComponent],
+  imports: [],
   templateUrl: './info-game.component.html',
   styleUrl: './info-game.component.css'
 })
