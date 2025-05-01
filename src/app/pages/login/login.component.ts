b7005dd2347d4202a3eb41d0067bd3bd78ddf3c2
EduardoFernandes1002 <fernandeseduardo1002@gmail.com>
2025-04-30 21:06:57 -0300

    Working Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private AuthUrlApi = 'http://localhost:8080/autenticar';
+  public username: string = '';
+  public password: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  autenticarLogin() {
+    const params = new HttpParams()
+      .set('login', this.username)
+      .set('senhaUsuario', this.password);
+  
+    this.http.post(`${this.AuthUrlApi}/login`, params.toString(), {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    }).subscribe({
+      next: (response) => console.log('Login bem-sucedido:', response),
+      error: (error) => {
+        console.error('Erro completo:', error);
+        console.error('Detalhes do erro:', error.error);
+      }
+    });
+  }
 }
