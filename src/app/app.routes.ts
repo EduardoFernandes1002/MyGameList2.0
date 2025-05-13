f528eccb9e0bf2af6a9c490aefcb3d3df7b184b1
EduardoFernandes1002 <FernandesEduardo1002@gmail.com>
2025-05-13 17:10:17 -0300

    auths and comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,22 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { PerfilComponent } from './pages/perfil/perfil.component';
-import { InfoGameComponent } from './pages/info-game/info-game.component';
-import { RankComponent } from './pages/rank/rank.component';
-import { AdminComponent } from './pages/admin/admin.component';
-import { RecomendedComponent } from './pages/recomended/recomended.component';
-import { CategoryComponent } from './pages/category/category.component';
-import { HelpComponent } from './pages/help/help.component';
-import { DiscoverComponent } from './pages/discover/discover.component';
-import { ConfigComponent } from './pages/config/config.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/general/home/home.component';
+import { PerfilComponent } from './pages/user/perfil/perfil.component';
+import { InfoGameComponent } from './pages/game/info-game/info-game.component';
+import { RankComponent } from './pages/game/rank/rank.component';
+import { AdminComponent } from './pages/user/admin/admin.component';
+import { RecomendedComponent } from './pages/game/recomended/recomended.component';
+import { CategoryComponent } from './pages/general/category/category.component';
+import { HelpComponent } from './pages/general/help/help.component';
+import { DiscoverComponent } from './pages/game/discover/discover.component';
+import { ConfigComponent } from './pages/user/config/config.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { RegisterComponent } from './pages/user/register/register.component';
+import { authGuard } from './guard/auth/auth.guard';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
-  { path: 'perfil', component: PerfilComponent, title: 'Perfil' },
+  { path: 'perfil/:nomeUsuario', component: PerfilComponent, title: 'Perfil', canActivate: [authGuard] },
   { path: 'info/:nomeJogo', component: InfoGameComponent, title: 'Informações' },
   { path: 'rank', component: RankComponent, title: 'Ranking' },
   { path: 'admin', component: AdminComponent, title: 'Administração' },
@@ -23,7 +24,7 @@ export const routes: Routes = [
   { path: 'categoria', component: CategoryComponent, title: 'Categoria' },
   { path: 'ajuda', component: HelpComponent, title: 'Ajuda/FAQ' },
   { path: 'descoberta', component: DiscoverComponent, title: 'Descoberta' },
-  { path: 'config', component: ConfigComponent, title: 'Configuração' },
+  { path: 'config/:nomeUsuario', component: ConfigComponent, title: 'Configuração', canActivate: [authGuard] },
   { path: 'login', component: LoginComponent, title: 'Login' },
-  { path: 'registro', component: RegisterComponent, title: 'Registro' }
+  { path: 'registro', component: RegisterComponent, title: 'Registro', canActivate: [!authGuard] },
 ];
