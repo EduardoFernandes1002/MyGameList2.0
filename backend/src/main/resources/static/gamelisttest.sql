97a7a301ec358321622468bb6c8ad454742578ff
EduardoFernandes1002 <FernandesEduardo1002@gmail.com>
2025-06-25 23:00:33 -0300

    sql

--- backend/src/main/resources/static/gamelisttest.sql
+++ backend/src/main/resources/static/gamelisttest.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: gamelisttest
 -- ------------------------------------------------------
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `avaliacao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `avaliacao` (
   `id_avaliacao` int(11) NOT NULL AUTO_INCREMENT,
   `id_jogo` int(11) NOT NULL,
@@ -54,7 +54,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `desenvolvedora`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `desenvolvedora` (
   `id_desenvolvedora` int(11) NOT NULL AUTO_INCREMENT,
   `nm_desenvolvedora` varchar(100) NOT NULL,
@@ -79,7 +79,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `distribuidora`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `distribuidora` (
   `id_distribuidora` int(11) NOT NULL AUTO_INCREMENT,
   `nm_distribuidora` varchar(100) NOT NULL,
@@ -104,7 +104,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `genero`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `genero` (
   `id_genero` int(11) NOT NULL AUTO_INCREMENT,
   `nm_genero` varchar(100) NOT NULL,
@@ -129,7 +129,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `genero_do_jogo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `genero_do_jogo` (
   `id_jogo` int(11) NOT NULL,
   `id_genero` int(11) NOT NULL,
@@ -147,7 +147,7 @@ CREATE TABLE `genero_do_jogo` (
 
 LOCK TABLES `genero_do_jogo` WRITE;
 /*!40000 ALTER TABLE `genero_do_jogo` DISABLE KEYS */;
-INSERT INTO `genero_do_jogo` VALUES (1,1),(1,2),(2,2),(2,3),(3,1),(3,3),(4,5),(4,12),(5,4),(5,9),(6,2),(6,14),(7,1),(7,12),(8,7),(8,8),(9,1),(9,13),(10,1),(10,5),(11,1),(11,9),(12,4),(12,9);
+INSERT INTO `genero_do_jogo` VALUES (2,2),(2,3),(3,1),(3,3),(4,5),(4,12),(5,4),(5,9),(6,2),(6,14),(7,1),(7,12),(8,7),(8,8),(9,1),(9,13),(10,1),(10,5),(11,1),(11,9),(12,4),(12,9);
 /*!40000 ALTER TABLE `genero_do_jogo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -157,7 +157,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `jogo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `jogo` (
   `id_jogo` int(11) NOT NULL AUTO_INCREMENT,
   `id_distribuidora` int(11) NOT NULL,
@@ -183,7 +183,7 @@ CREATE TABLE `jogo` (
 
 LOCK TABLES `jogo` WRITE;
 /*!40000 ALTER TABLE `jogo` DISABLE KEYS */;
-INSERT INTO `jogo` VALUES (1,1,1,'TesteJogo','TesteSinopse','2000-01-01','https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Test.svg/1200px-Test.svg.png',9.9),(2,2,2,'The Isle','Jogo de dino legal','2015-11-01','https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/376210/capsule_616x353.jpg?t=1653237914',10.0),(3,3,3,'The Legend of Zelda: Breath of the Wild','Em um reino devastado, Link deve explorar Hyrule para derrotar Calamity Ganon.','2017-03-03','https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_656/b_white/f_auto/q_auto/ncom/software/switch/70010000000025/7137262b5a64d921e193653f8aa0b722925abc5680380ca0e18a5cfd91697f58',8.0),(4,4,4,'Elden Ring','Um RPG de ação em um mundo aberto criado por Hidetaka Miyazaki e George R.R. Martin.','2022-02-25','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Nou4oHurJxom1LW1QZvXbvtsRRzpNTD28g&s',8.7),(5,5,5,'Fortnite','Um jogo de battle royale onde 100 jogadores lutam até restar um vencedor.','2017-07-25','https://cdn2.unrealengine.com/14br-consoles-1920x1080-wlogo-1920x1080-432974386.jpg',6.0),(6,6,6,'Minecraft','Construa, explore e sobreviva em um mundo gerado proceduralmente.','2011-11-18','https://image.api.playstation.com/vulcan/img/cfn/11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.png',8.7),(7,7,7,'Cyberpunk 2077','Em Night City, um mercenário busca um implante único que concede a imortalidade.','2020-12-10','https://image.api.playstation.com/vulcan/ap/rnd/202209/2703/8ZqdXQN9qkBGLaO5R9dUQJGQ.png',8.0),(8,8,8,'FIFA 23','O último jogo da franquia FIFA com times, estádios e modos atualizados.','2022-09-30','https://image.api.playstation.com/vulcan/ap/rnd/202207/1219/5fz6p4zQmQ5SqUgjKu6GqMX3.png',6.0),(9,9,9,'God of War: Ragnarök','Kratos e Atreus enfrentam o Fimbulwinter e a chegada do Ragnarök.','2022-11-09','https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png',9.0),(10,10,10,'Hades','Um roguelike onde Zagreus tenta escapar do Submundo grego.','2020-09-17','https://image.api.playstation.com/vulcan/ap/rnd/202008/1321/5m0Tz5MELh6DdQ43SJtBw4GJ.png',7.1),(11,11,11,'Among Us','Um jogo de dedução social onde tripulantes precisam encontrar o impostor.','2018-06-15','https://image.api.playstation.com/vulcan/ap/rnd/202011/1300/0f8s6H8L46AJTbx9qQFnk1Zt.png',6.4),(12,8,12,'Apex Legends','Um battle royale futurista com personagens únicos e habilidades especiais.','2019-02-04','https://image.api.playstation.com/vulcan/ap/rnd/202206/0720/eEczyEMDd2B8PfNAh4hBgl67.png',4.0);
+INSERT INTO `jogo` VALUES (2,2,2,'The Isle','Jogo de dino legal','2015-11-01','https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/376210/capsule_616x353.jpg?t=1653237914',10.0),(3,3,3,'The Legend of Zelda: Breath of the Wild','Em um reino devastado, Link deve explorar Hyrule para derrotar Calamity Ganon.','2017-03-03','https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_656/b_white/f_auto/q_auto/ncom/software/switch/70010000000025/7137262b5a64d921e193653f8aa0b722925abc5680380ca0e18a5cfd91697f58',8.0),(4,4,4,'Elden Ring','Um RPG de ação em um mundo aberto criado por Hidetaka Miyazaki e George R.R. Martin.','2022-02-25','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Nou4oHurJxom1LW1QZvXbvtsRRzpNTD28g&s',8.7),(5,5,5,'Fortnite','Um jogo de battle royale onde 100 jogadores lutam até restar um vencedor.','2017-07-25','https://cdn2.unrealengine.com/14br-consoles-1920x1080-wlogo-1920x1080-432974386.jpg',6.0),(6,6,6,'Minecraft','Construa, explore e sobreviva em um mundo gerado proceduralmente.','2011-11-18','https://image.api.playstation.com/vulcan/img/cfn/11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.png',8.7),(7,7,7,'Cyberpunk 2077','Em Night City, um mercenário busca um implante único que concede a imortalidade.','2020-12-10','https://image.api.playstation.com/vulcan/ap/rnd/202209/2703/8ZqdXQN9qkBGLaO5R9dUQJGQ.png',8.0),(8,8,8,'FIFA 23','O último jogo da franquia FIFA com times, estádios e modos atualizados.','2022-09-30','https://image.api.playstation.com/vulcan/ap/rnd/202207/1219/5fz6p4zQmQ5SqUgjKu6GqMX3.png',6.0),(9,9,9,'God of War: Ragnarök','Kratos e Atreus enfrentam o Fimbulwinter e a chegada do Ragnarök.','2022-11-09','https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png',9.0),(10,10,10,'Hades','Um roguelike onde Zagreus tenta escapar do Submundo grego.','2020-09-17','https://image.api.playstation.com/vulcan/ap/rnd/202008/1321/5m0Tz5MELh6DdQ43SJtBw4GJ.png',7.1),(11,11,11,'Among Us','Um jogo de dedução social onde tripulantes precisam encontrar o impostor.','2018-06-15','https://image.api.playstation.com/vulcan/ap/rnd/202011/1300/0f8s6H8L46AJTbx9qQFnk1Zt.png',6.4),(12,8,12,'Apex Legends','Um battle royale futurista com personagens únicos e habilidades especiais.','2019-02-04','https://image.api.playstation.com/vulcan/ap/rnd/202206/0720/eEczyEMDd2B8PfNAh4hBgl67.png',4.0);
 /*!40000 ALTER TABLE `jogo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,7 +193,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `jogo_adicionado`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `jogo_adicionado` (
   `id_lista` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
@@ -213,7 +213,7 @@ CREATE TABLE `jogo_adicionado` (
 
 LOCK TABLES `jogo_adicionado` WRITE;
 /*!40000 ALTER TABLE `jogo_adicionado` DISABLE KEYS */;
-INSERT INTO `jogo_adicionado` VALUES (1,1,4),(1,1,5),(1,1,7),(1,1,8),(1,1,10),(1,1,2),(1,1,1);
+INSERT INTO `jogo_adicionado` VALUES (1,1,4),(1,1,5),(1,1,7),(1,1,8),(1,1,10),(1,1,2);
 /*!40000 ALTER TABLE `jogo_adicionado` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -223,12 +223,12 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `lista`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lista` (
   `id_lista` int(11) NOT NULL AUTO_INCREMENT,
   `nm_lista` varchar(45) NOT NULL,
   PRIMARY KEY (`id_lista`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -237,7 +237,7 @@ CREATE TABLE `lista` (
 
 LOCK TABLES `lista` WRITE;
 /*!40000 ALTER TABLE `lista` DISABLE KEYS */;
-INSERT INTO `lista` VALUES (1,'Geral'),(2,'Jogando'),(3,'Completo'),(4,'Abandonado'),(5,'Pausado'),(6,'Desejo');
+INSERT INTO `lista` VALUES (1,'Geral'),(2,'Jogando'),(3,'Completo'),(4,'Abandonado'),(5,'Pausado'),(6,'Desejo'),(7,'Favoritos');
 /*!40000 ALTER TABLE `lista` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -247,7 +247,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `modo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `modo` (
   `id_modo` int(11) NOT NULL AUTO_INCREMENT,
   `nm_modo` varchar(100) NOT NULL,
@@ -272,7 +272,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `modo_de_jogo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `modo_de_jogo` (
   `id_jogo` int(11) NOT NULL,
   `id_modo` int(11) NOT NULL,
@@ -290,7 +290,7 @@ CREATE TABLE `modo_de_jogo` (
 
 LOCK TABLES `modo_de_jogo` WRITE;
 /*!40000 ALTER TABLE `modo_de_jogo` DISABLE KEYS */;
-INSERT INTO `modo_de_jogo` VALUES (1,1),(1,3),(1,4),(2,2),(2,4),(2,5);
+INSERT INTO `modo_de_jogo` VALUES (2,2),(2,4),(2,5);
 /*!40000 ALTER TABLE `modo_de_jogo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -300,7 +300,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `permissao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `permissao` (
   `id_permissao` int(11) NOT NULL AUTO_INCREMENT,
   `nm_permissao` varchar(255) DEFAULT NULL,
@@ -325,7 +325,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `plataforma`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `plataforma` (
   `id_plataforma` int(11) NOT NULL AUTO_INCREMENT,
   `nm_plataforma` varchar(100) NOT NULL,
@@ -350,7 +350,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `plataforma_de_jogo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `plataforma_de_jogo` (
   `id_plataforma` int(11) NOT NULL,
   `id_jogo` int(11) NOT NULL,
@@ -368,7 +368,6 @@ CREATE TABLE `plataforma_de_jogo` (
 
 LOCK TABLES `plataforma_de_jogo` WRITE;
 /*!40000 ALTER TABLE `plataforma_de_jogo` DISABLE KEYS */;
-INSERT INTO `plataforma_de_jogo` VALUES (1,1),(1,2),(2,1);
 /*!40000 ALTER TABLE `plataforma_de_jogo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -378,7 +377,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `id_permissao` int(11) NOT NULL,
@@ -404,7 +403,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,2,'AokiFernandes','Aoki','duduzebas@gmail.com','123456789',NULL,NULL),(2,2,'ThiagoDosSantos','Sr_Xurineio','ThiagoDosSantos','987654321',NULL,NULL),(3,1,'Teste','Teste','Teste@gmail.com','1231',NULL,NULL),(4,1,'Marcos Pedro','Marquin','MarcosPedro12@gmail.com','MarcosPedro12',NULL,NULL),(5,1,'Guilherme Laurindo','Abobora12','LaurindoG@gmail.com','Guilherme12',NULL,NULL),(10,1,NULL,NULL,'{\"senhaUsuario\":\"\"}',NULL,NULL,NULL);
+INSERT INTO `usuario` VALUES (1,2,'AokiFernandes','Aoki','duduzebas@gmail.com','123456789',NULL,NULL),(2,2,'ThiagoDosSantos','Sr_Xurineio','ThiagoDosSantos','987654321',NULL,NULL),(4,1,'Marcos Pedro','Marquin','MarcosPedro12@gmail.com','MarcosPedro12',NULL,NULL),(5,1,'Guilherme Laurindo','Abobora12','LaurindoG@gmail.com','Guilherme12',NULL,NULL);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -417,4 +416,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-24 19:28:14
+-- Dump completed on 2025-06-25 23:00:25
