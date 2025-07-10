80afc37a9820ba9e46a3e84cd2f5f485ad5131ea
EduardoFernandes1002 <fernandeseduardo1002@gmail.com>
2025-07-09 21:13:15 -0300

    Update gamelisttest.sql

--- backend/src/main/resources/static/gamelisttest.sql
+++ backend/src/main/resources/static/gamelisttest.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: gamelisttest
+-- Host: 127.0.0.1    Database: gamelist
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `avaliacao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `avaliacao` (
   `id_avaliacao` int(11) NOT NULL AUTO_INCREMENT,
   `id_jogo` int(11) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE `avaliacao` (
   KEY `t_avaliacao_ibfk_4` (`id_jogo`),
   CONSTRAINT `t_avaliacao_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
   CONSTRAINT `t_avaliacao_ibfk_4` FOREIGN KEY (`id_jogo`) REFERENCES `jogo` (`id_jogo`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `avaliacao` (
 
 LOCK TABLES `avaliacao` WRITE;
 /*!40000 ALTER TABLE `avaliacao` DISABLE KEYS */;
-INSERT INTO `avaliacao` VALUES (1,2,1,'TesteComentario',7.0,'2025-05-13','2025-05-13'),(2,2,2,'Tão bom que jogo até quando não deveria',10.0,'2025-05-22','2025-05-22');
+INSERT INTO `avaliacao` VALUES (1,2,1,'Jogo de dinossauro que o Sr_Xurineio é viciadão!',7.0,'2025-06-26','2025-06-26'),(2,2,2,'Tão bom que jogo até quando não deveria',10.0,'2025-05-22','2025-05-22'),(11,5,1,'Jogo ruim! apenas!',4.0,'2025-06-26','2025-06-26'),(12,6,1,'Melhor jogo de sobrevivencia da historia!',10.0,'2025-06-26','2025-06-26'),(13,10,1,'Não conheço mas deve ser legal, nota 8.',8.0,'2025-06-26','2025-06-26'),(14,4,1,'Curto um soulslike, mas meu hype acabou!',7.0,'2025-06-30','2025-07-01'),(15,11,1,'So vale a pena com amigos!\n',6.0,'2025-06-30','2025-07-01');
 /*!40000 ALTER TABLE `avaliacao` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,7 +54,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `desenvolvedora`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `desenvolvedora` (
   `id_desenvolvedora` int(11) NOT NULL AUTO_INCREMENT,
   `nm_desenvolvedora` varchar(100) NOT NULL,
@@ -79,7 +79,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `distribuidora`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `distribuidora` (
   `id_distribuidora` int(11) NOT NULL AUTO_INCREMENT,
   `nm_distribuidora` varchar(100) NOT NULL,
@@ -104,7 +104,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `genero`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `genero` (
   `id_genero` int(11) NOT NULL AUTO_INCREMENT,
   `nm_genero` varchar(100) NOT NULL,
@@ -129,7 +129,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `genero_do_jogo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `genero_do_jogo` (
   `id_jogo` int(11) NOT NULL,
   `id_genero` int(11) NOT NULL,
@@ -157,7 +157,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `jogo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `jogo` (
   `id_jogo` int(11) NOT NULL AUTO_INCREMENT,
   `id_distribuidora` int(11) NOT NULL,
@@ -183,7 +183,7 @@ CREATE TABLE `jogo` (
 
 LOCK TABLES `jogo` WRITE;
 /*!40000 ALTER TABLE `jogo` DISABLE KEYS */;
-INSERT INTO `jogo` VALUES (2,2,2,'The Isle','Jogo de dino legal','2015-11-01','https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/376210/capsule_616x353.jpg?t=1653237914',10.0),(3,3,3,'The Legend of Zelda: Breath of the Wild','Em um reino devastado, Link deve explorar Hyrule para derrotar Calamity Ganon.','2017-03-03','https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_656/b_white/f_auto/q_auto/ncom/software/switch/70010000000025/7137262b5a64d921e193653f8aa0b722925abc5680380ca0e18a5cfd91697f58',8.0),(4,4,4,'Elden Ring','Um RPG de ação em um mundo aberto criado por Hidetaka Miyazaki e George R.R. Martin.','2022-02-25','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Nou4oHurJxom1LW1QZvXbvtsRRzpNTD28g&s',8.7),(5,5,5,'Fortnite','Um jogo de battle royale onde 100 jogadores lutam até restar um vencedor.','2017-07-25','https://cdn2.unrealengine.com/14br-consoles-1920x1080-wlogo-1920x1080-432974386.jpg',6.0),(6,6,6,'Minecraft','Construa, explore e sobreviva em um mundo gerado proceduralmente.','2011-11-18','https://image.api.playstation.com/vulcan/img/cfn/11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.png',8.7),(7,7,7,'Cyberpunk 2077','Em Night City, um mercenário busca um implante único que concede a imortalidade.','2020-12-10','https://image.api.playstation.com/vulcan/ap/rnd/202209/2703/8ZqdXQN9qkBGLaO5R9dUQJGQ.png',8.0),(8,8,8,'FIFA 23','O último jogo da franquia FIFA com times, estádios e modos atualizados.','2022-09-30','https://image.api.playstation.com/vulcan/ap/rnd/202207/1219/5fz6p4zQmQ5SqUgjKu6GqMX3.png',6.0),(9,9,9,'God of War: Ragnarök','Kratos e Atreus enfrentam o Fimbulwinter e a chegada do Ragnarök.','2022-11-09','https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png',9.0),(10,10,10,'Hades','Um roguelike onde Zagreus tenta escapar do Submundo grego.','2020-09-17','https://image.api.playstation.com/vulcan/ap/rnd/202008/1321/5m0Tz5MELh6DdQ43SJtBw4GJ.png',7.1),(11,11,11,'Among Us','Um jogo de dedução social onde tripulantes precisam encontrar o impostor.','2018-06-15','https://image.api.playstation.com/vulcan/ap/rnd/202011/1300/0f8s6H8L46AJTbx9qQFnk1Zt.png',6.4),(12,8,12,'Apex Legends','Um battle royale futurista com personagens únicos e habilidades especiais.','2019-02-04','https://image.api.playstation.com/vulcan/ap/rnd/202206/0720/eEczyEMDd2B8PfNAh4hBgl67.png',4.0);
+INSERT INTO `jogo` VALUES (2,2,2,'The Isle','The Isle é um jogo de sobrevivência em mundo aberto e horror, onde os jogadores devem sobreviver em uma ilha habitada por dinossauros. O jogo oferece uma variedade de criaturas jogáveis, desde herbívoros a carnívoros, e desafios como fome, sede e ameaças de outros jogadores e dinossauros. A experiência se foca na sobrevivência em um ambiente hostil, com exploração, caça e a necessidade de adaptação para prosperar. ','2015-11-01','https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/376210/capsule_616x353.jpg?t=1653237914',10.0),(3,3,3,'The Legend of Zelda: Breath of the Wild','Em um reino devastado, Link deve explorar Hyrule para derrotar Calamity Ganon.','2017-03-03','https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_656/b_white/f_auto/q_auto/ncom/software/switch/70010000000025/7137262b5a64d921e193653f8aa0b722925abc5680380ca0e18a5cfd91697f58',8.0),(4,4,4,'Elden Ring','Um RPG de ação em um mundo aberto criado por Hidetaka Miyazaki e George R.R. Martin.','2022-02-25','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5Nou4oHurJxom1LW1QZvXbvtsRRzpNTD28g&s',8.7),(5,5,5,'Fortnite','Um jogo de battle royale onde 100 jogadores lutam até restar um vencedor.','2017-07-25','https://cms-assets.unrealengine.com/cm6l5gfpm05kr07my04cqgy2x/cm9wwjg2r2n9e08n4hrfeouns',6.0),(6,6,6,'Minecraft','Construa, explore e sobreviva em um mundo gerado proceduralmente.','2011-11-18','https://image.api.playstation.com/vulcan/img/cfn/11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.png',8.7),(7,7,7,'Cyberpunk 2077','Em Night City, um mercenário busca um implante único que concede a imortalidade.','2020-12-10','https://www.cyberpunk.net/build/images/social-thumbnail-en-ddcf4d23.jpg',8.0),(8,8,8,'FIFA 23','O FIFA 23 é o último jogo da franquia FIFA desenvolvido pela Electronic Arts, antes da mudança para a EA Sports FC. O jogo traz inovações como a tecnologia HyperMotion2, que oferece animações mais realistas, e a inclusão de torneios da FIFA World Cup™ masculina e feminina, além de times femininos pela primeira vez. O jogo também apresenta recursos como crossplay e melhorias nos modos Ultimate Team e Carreira. ','2022-09-30','https://assets.nintendo.com/image/upload/q_auto/f_auto/ncom/software/switch/70010000047794/40f9e2f6d344d2607d4be72c6c4ebd3c18717ff87a56d6bbc303c42e58dd570d',6.0),(9,9,9,'God of War: Ragnarök','God of War Ragnarök dá continuidade à história de Kratos e Atreus, ambientada após os eventos do jogo anterior, em meio ao Fimbulwinter, que precede o Ragnarök. A dupla busca respostas nos Nove Reinos enquanto se preparam para o confronto final profetizado, que pode levar ao fim do mundo. ','2022-11-09','https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png',9.0),(10,10,10,'Hades','Um roguelike onde Zagreus tenta escapar do Submundo grego.','2020-09-17','https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiDmuxS38ekrs51ij4WHn9A1mYq1lfonwn52hLcCDtq3O5qqjLkUPd5dBa7g3Gxc-rnhaAmLcHMl1LaVbCgt4TaHPr36C1JdCYKX49oEHLBzIjcwX3SK37hZAX1pSlPoFVct28Yj9JKepY/w1200-h630-p-k-no-nu/H2x1_NSwitchDS_Hades.png',7.1),(11,11,11,'Among Us','Um jogo de dedução social onde tripulantes precisam encontrar o impostor.','2018-06-15','https://upload.wikimedia.org/wikipedia/pt/7/72/AmongUs_CoverArt.jpg',6.4),(12,8,12,'Apex Legends','Um battle royale futurista com personagens únicos e habilidades especiais.','2019-02-04','https://image.api.playstation.com/vulcan/ap/rnd/202206/0720/eEczyEMDd2B8PfNAh4hBgl67.png',4.0);
 /*!40000 ALTER TABLE `jogo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -193,7 +193,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `jogo_adicionado`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `jogo_adicionado` (
   `id_lista` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
@@ -213,7 +213,7 @@ CREATE TABLE `jogo_adicionado` (
 
 LOCK TABLES `jogo_adicionado` WRITE;
 /*!40000 ALTER TABLE `jogo_adicionado` DISABLE KEYS */;
-INSERT INTO `jogo_adicionado` VALUES (1,1,4),(1,1,5),(1,1,7),(1,1,8),(1,1,10),(1,1,2);
+INSERT INTO `jogo_adicionado` VALUES (1,1,4),(1,1,5),(1,1,7),(1,1,8),(1,1,10),(1,1,2),(2,1,2),(4,1,5),(5,1,6),(7,1,6),(3,1,10),(7,1,10),(7,1,8);
 /*!40000 ALTER TABLE `jogo_adicionado` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -223,7 +223,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `lista`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `lista` (
   `id_lista` int(11) NOT NULL AUTO_INCREMENT,
   `nm_lista` varchar(45) NOT NULL,
@@ -247,7 +247,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `modo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `modo` (
   `id_modo` int(11) NOT NULL AUTO_INCREMENT,
   `nm_modo` varchar(100) NOT NULL,
@@ -272,7 +272,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `modo_de_jogo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `modo_de_jogo` (
   `id_jogo` int(11) NOT NULL,
   `id_modo` int(11) NOT NULL,
@@ -300,7 +300,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `permissao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `permissao` (
   `id_permissao` int(11) NOT NULL AUTO_INCREMENT,
   `nm_permissao` varchar(255) DEFAULT NULL,
@@ -325,7 +325,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `plataforma`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `plataforma` (
   `id_plataforma` int(11) NOT NULL AUTO_INCREMENT,
   `nm_plataforma` varchar(100) NOT NULL,
@@ -350,7 +350,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `plataforma_de_jogo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `plataforma_de_jogo` (
   `id_plataforma` int(11) NOT NULL,
   `id_jogo` int(11) NOT NULL,
@@ -377,7 +377,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `id_permissao` int(11) NOT NULL,
@@ -416,4 +416,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-25 23:00:25
+-- Dump completed on 2025-07-09 21:12:24
